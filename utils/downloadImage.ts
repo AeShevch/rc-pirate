d2a3e37100c6b8b85da0f7609bfc2ebc0e4feb9b
AeShevch <shevch.ae@gmail.com>
2023-02-08 22:24:51 +0400

    feat: update download time

--- utils/downloadImage.ts
+++ utils/downloadImage.ts
@@ -2,9 +2,6 @@ import axios from "axios";
 import https from "https";
 import fs from "fs";
 
-axios.defaults.timeout = 3000;
-axios.defaults.httpsAgent = new https.Agent({ keepAlive: true });
-
 export const downloadImage = async (
   url: string,
   path: string
